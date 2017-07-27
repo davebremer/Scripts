1e1fa0adb61359542b43fc944131e8ef7875f8f1
Dave Bremer <dave@bremer.co.nz>
2017-07-27 16:46:12 +1200

    putting get-lastlogontoad as a script
    Removed from ADTools as we can use the lastlogondate field. This is pretty slow - but is up to date. The lastlogondate can take severla days to replicate properly

---
+++ get-lastLogonToAD.ps1
@@ -0,0 +1,82 @@
+﻿function Get-LastLogonToAD {
+<#
+.SYNOPSIS
+ Gets the last login time for a username by querying all domain controllers
+
+.DESCRIPTION
+ Gets the last login time for a username by querying all domain controllers
+
+.PARAMETER UserName
+Username to search
+
+
+.EXAMPLE
+Get-LastLogonToAD -username daveb
+Returns the last login time for daveb
+
+.NOTES
+ Author: Dave Bremer
+ 
+
+#>
+    [cmdletBinding()]
+    Param ([Parameter (
+            Mandatory=$True,
+            ValueFromPipeLine = $TRUE,
+            ValueFromPipelineByPropertyName = $TRUE
+                )]
+            [ValidateNotNullOrEmpty()]
+            [Alias('SamID')]
+            [string] $UserName
+            )
+
+BEGIN{
+    $dc = Get-ADDomainController -Filter * | Select-Object name
+    $tot = $dc.count
+    $user = $null
+   
+}
+
+PROCESS {
+     write-verbose "looking for $username"     
+    $counter=0 #for progress bar
+    foreach ($c in $dc) {
+        #draw progress bar
+        $counter+=1
+        $prog=[system.math]::round($counter/$tot*100,2)
+        write-progress -activity ("Server {0}. {1} servers left to check" -f $c.name,($tot-$counter)) -status "$prog% Complete:" -percentcomplete $prog;
+        
+        #search server
+        try{
+            Write-verbose ("Server: {0}, User {1}" -f $c.name,$username)
+            $temp = get-aduser -Server $c.name -identity $username -Properties lastlogon
+            Write-Verbose $temp
+
+            Write-verbose ("{0} LastLogon {1}" -f $c.name,[DateTime]::FromFileTime($temp.lastlogon))
+
+            if ($user.LastLogon -lt $temp.lastlogon) { 
+                $user = $temp.PsObject.Copy() 
+                $recent = $c.name
+            }
+        } catch {
+        $recent = $null
+        Write-verbose ("Error getting from Server: {0}" -f $c.name)
+        
+        }
+    
+
+    }
+
+          
+    $prop = @{"LatestDC" = $recent;
+            "LastLogon" = [DateTime]::FromFileTime($user.lastlogon)
+            } # prop
+                               
+
+    $obj = New-Object -TypeName PSObject -Property $prop
+    $obj.psobject.typenames.insert(0, 'daveb.adtools.lastlogon')
+    Write-Output $obj 
+}
+END{}
+}
+
